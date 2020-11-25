97af591c6b54d5b869f565ee848928cd282a89c4
Pradeepa Vasudevan <rengan76@gmail.com>
2020-11-25 18:40:31 -0500

    added config

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const url = 'http//localhost:9000/api/v1';
+
+const headers = {
+    "accept" : "application/json"
+};
+
+module.exports = {
+    url,
+    headers
+};
+
---
+++ config/database.js
@@ -0,0 +1,10 @@
+var config = {
+    port : 3000,
+    HOST: "ze25twcox4llx7t8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    USER: "nvst6jyetj7su98w",
+    PASSWORD: "zemf1tqsrt4nbux3",
+    DB: "/exc_production"
+   
+};
+
+module.exports = config;
