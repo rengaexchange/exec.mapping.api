f6a4854d676655e1c850a0dd0e13c0ea1ff76079
Pradeepa Vasudevan <rengan76@gmail.com>
2020-12-03 17:17:23 -0500

    Removed console log

--- src/controllers/brandingController.js
+++ src/controllers/brandingController.js
@@ -6,7 +6,6 @@ const brandData = require('../services/brandingService').get;
 async function getData (req, res, cb) {
     let brand = req.query.brand;
     let limit = req.query.limit;
-    console.log("limit" + limit);
     const data = await brandData(brand, limit);
     res.json(data);
   };
